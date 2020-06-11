515fe36b97eaa309b4c67541f053977c7a3d7efe
alfaone <alfaone12@gmail.com>
2020-06-10 22:05:00 -0500

    crud con subida de imagen 2

--- src/main/java/com/springboot/controllers/ClienteController.java
+++ src/main/java/com/springboot/controllers/ClienteController.java
@@ -1,6 +1,7 @@
 package com.springboot.controllers;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,9 +11,13 @@ import java.util.UUID;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -39,6 +44,26 @@ public class ClienteController {
 
 	@Autowired
 	private IClienteService clienteService;
+	
+	@GetMapping("/uploads/{filename:.+}")
+	public ResponseEntity<Resource> verFoto(@PathVariable String filename){
+		Path pathFoto = Paths.get("uploads").resolve(filename).toAbsolutePath();
+		Resource recurso = null;
+		try {
+			recurso = new UrlResource(pathFoto.toUri());
+			if(!recurso.exists() && !recurso.isReadable()) {
+				throw new RuntimeException("Error: No se puede cargar la imagen: " + pathFoto.toString());
+			}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return ResponseEntity.ok()
+				.header(HttpHeaders.CONTENT_DISPOSITION,"attachment;filename=\""+ recurso.getFilename()+"\"")
+				.body(recurso);
+	}
+	
 	
 	@GetMapping(value = "/ver/{id}")
 	public String ver(@PathVariable(value="id") Long id, Map<String,Object> model,RedirectAttributes flash) {
